46282e1d9fb1bf9a487dfcf55841e22e88f0d78f
Barney <kdjone0147@naver.com>
2017-05-30 15:03:13 +0900

    init Heroku

--- router/main.js
+++ router/main.js
@@ -2,35 +2,9 @@
 module.exports = function(app, fs)
 {
 
-    var fs = require('fs');    			// 파일시스템 모듈
-    var express = require('express');   // express 모듈
-    var app = express();                // express 객체 생성
-    var http = require('http');         // Http 웹 서버 모듈
-    var server = http.createServer(app);    // 서버 객체 생성
-
-    app.set('port',7080);      // 서버 포트 설정
-
-    server.listen(app.get('port'),function(){       // 서버 가동 
-         console.log('Express server listening on port ' + app.get('port'));
-    });
-    
-    app.get('/public/image/:name',function (req,res){     
-    var filename = req.params.name;
-    console.log(__dirname+'/public/images/'+filename);
-    fs.exists(__dirname+'/public/images/'+filename, function (exists) {
-        if (exists) {
-            fs.readFile(__dirname+'/public/images/'+filename, function (err,data){
-                    res.end(data);
-                });
-            } else {
-                res.end('file is not exists');
-            }
-        })
-    });
-
      app.get('/',function(req,res){
          res.render('index', {
-             title: "MY SHOPPINGMALL",
+             title: "MY HOMEPAGE",
              length: 5
          })
      });
