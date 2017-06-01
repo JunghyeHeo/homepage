19446d55c609620d191878a9b0274ebc44c4c695
Barney <kdjone0147@naver.com>
2017-06-01 15:02:23 +0900

    init Heroku

--- server.js
+++ server.js
@@ -5,6 +5,25 @@ var session = require('express-session');
 var fs = require("fs")
 var PORT = process.env.PORT || 3000;
 
+
+var server = app.listen(PORT, function(){
+ console.log("Express server has started on port 3000")
+});
+
+
+app.use(express.static('public'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+ extended: true
+}));
+
+app.use(session({
+ secret: '@#@$MYSIGN#@$#$',
+ resave: false,
+ saveUninitialized: true
+}));
+
 app.get('/main', function(req, res){
     fs.readFile( __dirname + "/../data/user.json", function(err, data){
         console.log(data);
@@ -18,8 +37,4 @@ app.get('/img', function(req, res){
         res.writeHead(200, {'content-type': 'text/html'});
         res.end(data);
     });
-});
-
-var server = app.listen(PORT, function(){
- console.log("Express server has started on port 3000")
 });
